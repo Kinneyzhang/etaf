854b338d34aebc66ae5ee9461a772a34b9db7dcd
Kinneyzhang <kinneyzhang666@gmail.com>
2025-09-25 14:39:12 +0800

    update etml-flex

--- tests/etml-flex-tests.el
+++ tests/etml-flex-tests.el
@@ -3,17 +3,21 @@
 (defun etml-flex-tests-item (num &rest kvs)
   (eval `(etml-flex-item
           :self (etml-block :content (format "etml flex test %s" num))
-          :border "grey" :padding t
+          :border t :padding t
           ,@kvs)))
 
 (etml-test-pop-buffer "*etml-flex-tests*"
   (insert
    (etml-flex-render
-    (etml-flex :items (list (etml-flex-tests-item 1 :shrink 1)
-                            (etml-flex-tests-item 2 :shrink 1)
-                            (etml-flex-tests-item 3 :shrink 1)
-                            (etml-flex-tests-item 4 :shrink 1)
-                            (etml-flex-tests-item 5 :shrink 1))))))
+    (etml-flex
+     :border t :padding '(3 . 1) :margin '(3 . 1)
+     :bgcolor '("#FFF9F0" . "#222222")
+     :width '(300)
+     :items (list (etml-flex-tests-item 1 :shrink 1)
+                  (etml-flex-tests-item 2 :shrink 1)
+                  (etml-flex-tests-item 3 :shrink 1)
+                  (etml-flex-tests-item 4 :shrink 1)
+                  (etml-flex-tests-item 5 :shrink 1))))))
 
 ;; test shrink
 
