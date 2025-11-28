c2bfd45a112a5f0d0ce54b5dbdd2e150b958099c
Kinneyzhang <kinneyzhang666@gmail.com>
2025-11-28 22:32:56 +0800

    update

--- etaf.el
+++ etaf.el
@@ -5,4 +5,12 @@
 (require 'etaf-layout)
 (require 'etaf-tailwind)
 
+(defun etaf-string (tml width &optional height)
+  (let* ((dom (etaf-tml-to-dom tml))
+         (cssom (etaf-css-build-cssom dom))
+         (render-tree (etaf-render-build-tree dom cssom))
+         (layout-tree (etaf-layout-build-tree
+                       render-tree (list :width width :height height))))
+    (etaf-layout-to-string layout-tree)))
+
 (provide 'etaf)
